306c32819536f4f14498c4b412fef40d395df11e
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-10-21 01:52:36 -0300

    feat: initial nestjs setup with auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { SafeUser } from './types/safe-user.type';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async registerUser(
+    @Body() registerUserDto: RegisterUserDto,
+  ): Promise<SafeUser> {
+    return await this.authService.registerUser(registerUserDto);
+  }
+
+  @Post('login')
+  async login(loginUserDto: LoginUserDto): Promise<{ accessToken: string }> {
+    return this.authService.login(loginUserDto);
+  }
+}
