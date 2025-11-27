4d9991eeb0c84c54af11d17e6572b1f6ea1983d7
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-11-27 00:00:59 -0300

    feat: add roles guard/decorator and prisma enum and artisan-status guard

--- src/common/enums/prisma.enum.ts
+++ src/common/enums/prisma.enum.ts
@@ -2,3 +2,4 @@ import { $Enums } from '@prisma/client';
 
 export type UserRoles = $Enums.UserRole;
 export type ArtisanStatus = $Enums.ArtisanStatus;
+export type ProductStatus = $Enums.ProductStatus;
