b5b5ee1fd946cfae445873506ff00055d1d5deaa
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-11-29 19:28:07 -0300

    feat: add route me and update register route endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { SafeUser } from './types/safe-user.type';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login.dto';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post('register')
   async registerUser(
     @Body() registerUserDto: RegisterUserDto,
   ): Promise<SafeUser> {
@@ -20,5 +22,10 @@ export class AuthController {
     @Body() loginUserDto: LoginUserDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('me')
+  async getUserInfo(@GetUser() user: User) {
+    return user;
   }
 }
