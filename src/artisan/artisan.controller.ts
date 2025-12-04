59a96c8c6d2050bd4c1175f2563a96ec25433341
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-12-04 01:11:30 -0300

    feat: add updateArtisanProfile route

--- src/artisan/artisan.controller.ts
+++ src/artisan/artisan.controller.ts
@@ -1,13 +1,25 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ArtisanService } from './artisan.service';
 import { JwtAuthGuard } from 'src/common/guards/auth.guard';
 import { CreateArtisanDto } from './dto/create-artisan.dto';
-import { Artisan, User, UserRole } from '@prisma/client';
+import { Artisan, ArtisanStatus, User, UserRole } from '@prisma/client';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { UpdateArtisanDto } from './dto/update-artisan.dto';
+import { ArtisanStatusGuard } from 'src/common/guards/artisan-status.guard';
+import { RequireArtisanStatus } from 'src/common/decorators/artisan-status.decorator';
+import { Public } from 'src/common/decorators/public.decorator';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
+@UseGuards(JwtAuthGuard, RolesGuard, ArtisanStatusGuard)
 @Controller('artisan')
 export class ArtisanController {
   constructor(private readonly artisanService: ArtisanService) {}
@@ -22,6 +34,7 @@ export class ArtisanController {
   }
 
   @Get(':artisanId')
+  @Public()
   async getArtisanProfile(
     @Param('artisanId') artisanId: string,
   ): Promise<Artisan> {
@@ -29,7 +42,23 @@ export class ArtisanController {
   }
 
   @Get(':artisanId/dashboard')
+  @Public()
   async getArtisanDashboard(@Param('artisanId') artisanId: string) {
     return await this.artisanService.getArtisanDashboard(artisanId);
+  }
+
+  @Roles(UserRole.ARTISAN)
+  @RequireArtisanStatus(ArtisanStatus.APPROVED)
+  @Put(':artisanId')
+  async updateArtisanProfile(
+    @Param('artisanId') artisanId: string,
+    @Body() updateArtisanDto: UpdateArtisanDto,
+    @GetUser() user: User,
+  ): Promise<Artisan> {
+    return await this.artisanService.updateArtisan(
+      artisanId,
+      user.id,
+      updateArtisanDto,
+    );
   }
 }
